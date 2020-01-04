9ef838835a6941a41e6a76d44fcaf034c63441f4
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-04 05:45:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,24 +18,19 @@ my_hash ={ :first =>2,
  
  
 
-def read_from_hash(hash, key)
+
   read_from_hash { "whale bone corset" => 5,
   "porcelain vase" => 2
   }
   read_from_hash["porcelain vase"] = 3
-  read_from_hash
-end
+ 
 
-def update_counting_hash
-  { :hash => 3
+update_counting_hash = { :hash => 3
   }
    update_counting_hash ["hash"] = 2
   if  update_counting_hash ["hash"]
       update_counting_hash ["hash"] += 1 
   else 
       update_counting_hash ["hash"] = 1 
-end
-  # given a hash an a key as parameters, return an updated hash
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
-end
+      end
+  
